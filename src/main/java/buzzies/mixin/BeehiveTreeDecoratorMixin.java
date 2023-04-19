ae39f27af47dc8d7b80acbcd505293114be80ad7
Redstonia <redstonia1234@gmail.com>
2023-04-19 17:27:53 +1200

    always_all functionality boop

--- src/main/java/buzzies/mixin/BeehiveTreeDecoratorMixin.java
+++ src/main/java/buzzies/mixin/BeehiveTreeDecoratorMixin.java
@@ -1,5 +1,10 @@
 package buzzies.mixin;
 
+import net.minecraft.block.BeehiveBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.gen.treedecorator.BeehiveTreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import buzzies.custom.BeenestAlwaysDecorator;
@@ -10,10 +15,15 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
 
 @Mixin(BeehiveTreeDecorator.class)
 public abstract class BeehiveTreeDecoratorMixin
 {
+    @Shadow @Final
+    private static Direction BEE_NEST_FACE;
     @Shadow @Final
     private float probability;
 
@@ -25,6 +35,29 @@ public abstract class BeehiveTreeDecoratorMixin
             if (BuzziesSettings.beenestGeneration.usesModifiedDecorator()) {
                 BeenestAlwaysDecorator.generate(generator);
             }
+        }
+    }
+
+    @Inject(
+            method = "generate",
+            at = @At(value = "INVOKE", target = "Ljava/util/Collections;shuffle(Ljava/util/List;)V"),
+            cancellable = true,
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void customGenerationLogic(TreeDecorator.Generator generator, CallbackInfo ci, Random random, List list, List list2, int i, List<BlockPos> list3) {
+        // would be nice to get rid of random, list, list2 and i, but I've no braincells to get this to work
+        if (BuzziesSettings.beenestGeneration == BuzziesSettings.BeenestGenerationOptions.ALWAYS_ALL) {
+            ci.cancel();
+            tryGenerateNestInSpots(generator, list3);
+        }
+    }
+
+    private void tryGenerateNestInSpots(TreeDecorator.Generator generator, List<BlockPos> nestSpots) {
+        List<BlockPos> beenestSpots = nestSpots.stream().filter(pos -> generator.isAir(pos) && generator.isAir((pos).offset(BEE_NEST_FACE))).toList();
+        // feels messy cause just stolen directly from mojank code
+        for (BlockPos beenestSpot : beenestSpots)
+        {
+            generator.replace(beenestSpot, Blocks.BEE_NEST.getDefaultState().with(BeehiveBlock.FACING, BEE_NEST_FACE));
         }
     }
 }
