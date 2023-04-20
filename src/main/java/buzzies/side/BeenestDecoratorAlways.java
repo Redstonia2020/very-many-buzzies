961dd3d3f482b6fa31ab4789609f6f03b7f1fa2d
Redstonia <redstonia1234@gmail.com>
2023-04-20 18:01:49 +1200

    quick cleanup in decoratorAlways

--- src/main/java/buzzies/side/BeenestDecoratorAlways.java
+++ src/main/java/buzzies/side/BeenestDecoratorAlways.java
@@ -4,9 +4,9 @@ import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import net.minecraft.world.gen.treedecorator.BeehiveTreeDecorator;
 
 public class BeenestDecoratorAlways {
-    private static final BeehiveTreeDecorator generator = new BeehiveTreeDecorator(1.0f);
+    private static final BeehiveTreeDecorator decorator = new BeehiveTreeDecorator(1.0f);
 
     public static void generate(TreeDecorator.Generator generator) {
-        BeenestDecoratorAlways.generator.generate(generator);
+        decorator.generate(generator);
     }
 }
