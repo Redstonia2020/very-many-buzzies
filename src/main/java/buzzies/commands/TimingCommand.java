cf417804730d7bd135b6fb096c9892e013503092
Redstonia <redstonia1234@gmail.com>
2023-04-29 16:45:26 +1200

    timings expecting and also lots of weird command rework

--- src/main/java/buzzies/commands/TimingCommand.java
+++ src/main/java/buzzies/commands/TimingCommand.java
@@ -5,65 +5,98 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
-import static buzzies.utils.CommandUtils.*;
 import static com.mojang.brigadier.arguments.StringArgumentType.*;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.*;
 import static net.minecraft.server.command.CommandManager.*;
 import static net.minecraft.util.Formatting.*;
+import static buzzies.utils.CommandUtils.*;
 
 public class TimingCommand {
     private static long gameTimeStart;
 
+    private static boolean isExpecting;
+    private static int expectedInterval;
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        LiteralArgumentBuilder<ServerCommandSource> builder = literal("timing")
-                .executes(context -> defaultMessage(context.getSource()))
+        LiteralArgumentBuilder<ServerCommandSource> baseCommand = literal("timing")
+                .executes(context -> defaultMessage(context.getSource())
+                )
                 .then(literal("start")
                         .then(argument("message", greedyString())
-                                .executes(context -> start(getString(context, "message"), context.getSource()))))
+                                .executes(c -> start(s(c), getString(c, "message"))
+                                )))
                 .then(literal("log")
                         .then(argument("message", greedyString())
-                                .executes(context -> log(getString(context, "message"), context.getSource()))))
+                                .executes(c -> log(s(c), getString(c, "message"))
+                                )))
                 .then(literal("restart")
                         .then(argument("message", greedyString())
-                                .executes(context -> restart(getString(context, "message"), context.getSource()))));
+                                .executes(c -> restart(s(c), getString(c, "message"))
+                                )))
+                .then(literal("expects")
+                        .then(argument("interval", integer(1))
+                                .executes(c -> setExpectInterval(s(c), getInteger(c, "interval"))
+                                ))
+                        .then(literal("none")
+                                .executes(c -> disableExpecting(s(c)))
+                        ));
 
-        dispatcher.register(builder);
+        dispatcher.register(baseCommand);
     }
 
-    private static int defaultMessage(ServerCommandSource source) {
-        source.sendMessage(Text.literal("* You are lost in the timing command"));
+    private static int defaultMessage(ServerCommandSource s) {
+        send(s, "* You seem kinda lost");
         return 1;
     }
 
-    private static int start(String message, ServerCommandSource source) {
-        String print = "[%s] 0 - %s".formatted(getGameTime(source), message);
-        broadcast(source, print, GREEN);
+    private static int start(ServerCommandSource s, String message) {
+        String print = "[%s] 0 - %s".formatted(getGameTime(s), message);
+        broadcast(s, print, GREEN);
 
-        gameTimeStart = getGameTime(source);
-
-        return 1;
-    }
-
-    private static int log(String message, ServerCommandSource source) {
-        String print = "[%s] %s - %s".formatted(getGameTime(source), getTimeSinceStart(source), message);
-        broadcast(source, print);
-
-        return 1;
-    }
-
-    private static int restart(String message, ServerCommandSource source) {
-        String print = "[%s] %s - %s".formatted(getGameTime(source), getTimeSinceStart(source), message);
-        broadcast(source, print, GREEN);
-
-        gameTimeStart = getGameTime(source);
+        gameTimeStart = getGameTime(s);
 
         return 1;
     }
 
-    private static long getGameTime(ServerCommandSource source) {
-        return source.getWorld().getTime();
+    private static int log(ServerCommandSource s, String message) {
+        String print = "[%s] %s - %s".formatted(getGameTime(s), getTimeSinceStart(s), message);
+        broadcast(s, print);
+
+        return 1;
     }
 
-    private static long getTimeSinceStart(ServerCommandSource source) {
-        return getGameTime(source) - gameTimeStart;
+    private static int restart(ServerCommandSource s, String message) {
+        long timeSinceStart = getTimeSinceStart(s);
+        if (!isExpecting) {
+            String print = "[%s] %s - %s".formatted(getGameTime(s), timeSinceStart, message);
+            broadcast(s, print, GREEN);
+        } else if (timeSinceStart != expectedInterval) {
+            String print = "[%s] %s (does not match expected interval %s) - %s".formatted(getGameTime(s), timeSinceStart, expectedInterval, message);
+            broadcast(s, print, RED);
+        }
+
+        gameTimeStart = getGameTime(s);
+        return 1;
+    }
+
+    private static int setExpectInterval(ServerCommandSource s, int interval) {
+        expectedInterval = interval;
+        isExpecting = true;
+        send(s, "You are expected to yell at me every %s ticks.".formatted(interval));
+        return 1;
+    }
+
+    private static int disableExpecting(ServerCommandSource s) {
+        isExpecting = false;
+        send(s, "Finally free of all the expectations.");
+        return 1;
+    }
+
+    private static long getGameTime(ServerCommandSource s) {
+        return s.getWorld().getTime();
+    }
+
+    private static long getTimeSinceStart(ServerCommandSource s) {
+        return getGameTime(s) - gameTimeStart;
     }
 }
--- src/main/java/buzzies/utils/CommandUtils.java
+++ src/main/java/buzzies/utils/CommandUtils.java
@@ -1,13 +1,24 @@
 package buzzies.utils;
 
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 public class CommandUtils {
-    public static void broadcast(ServerCommandSource source, String message, Formatting... format) {
+    public static ServerCommandSource s(CommandContext<ServerCommandSource> c) {
+        return c.getSource();
+    }
+
+    public static void send(ServerCommandSource source, String rawMessage, Formatting... format) {
+        Text message = Text.literal(rawMessage).formatted(format);
+        source.sendMessage(message);
+    }
+
+    public static void broadcast(ServerCommandSource source, String rawMessage, Formatting... format) {
+        Text message = Text.literal(rawMessage).formatted(format);
         PlayerManager playerManager = source.getServer().getPlayerManager();
-        playerManager.broadcast(Text.literal(message).formatted(format), false);
+        playerManager.broadcast(message, false);
     }
 }
