ca9827109e99e738a09cc1cdde7fc8d55dfa8437
Redstonia <redstonia1234@gmail.com>
2023-05-04 18:17:54 +1200

    funny zombie setting

---
+++ src/main/java/buzzies/mixins/ZombieEntityMixin.java
@@ -0,0 +1,19 @@
+package buzzies.mixins;
+
+import buzzies.settings.BuzziesSettings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.ZombieEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ZombieEntity.class)
+public class ZombieEntityMixin {
+    @Redirect(method = "convertInWater",
+              at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityType;DROWNED:Lnet/minecraft/entity/EntityType;"))
+    private EntityType drierDrown() {
+        if (BuzziesSettings.zombiesResistDrowning)
+            return EntityType.HUSK;
+        return EntityType.DROWNED;
+    }
+}
