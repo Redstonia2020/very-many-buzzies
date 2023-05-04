f753dcfc869539e9a8dc236b70e35b771f25268a
Redstonia <redstonia1234@gmail.com>
2023-05-05 11:20:26 +1200

    so turns out you do need ExecutionFunction to be able to throw command exceptions whoops

--- src/main/java/buzzies/commands/ExecutionFunction.java
+++ src/main/java/buzzies/commands/ExecutionFunction.java
@@ -1,6 +1,8 @@
 package buzzies.commands;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
 @FunctionalInterface
 public interface ExecutionFunction<T extends Execution> {
-    int run(T execution);
+    int run(T execution) throws CommandSyntaxException;
 }
